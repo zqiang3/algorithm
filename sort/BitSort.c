260d0a18a25f69e10d7c3950b477291be31b88e2
zqiang3 <zqiang3@126.com>
2018-11-14 11:25:18 +0800

    位排序

--- sort/BitSort.c
+++ sort/BitSort.c
@@ -2,43 +2,66 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
+#include <errno.h>
 
 #define MAX 1250000
-#define MAX_ONE_TIME 1000
-#define TIMES 10000
+#define MAX_ONE_TIME 10000
+#define TIMES 1000
 char buf[MAX];
+int arr[MAX_ONE_TIME];
 void print_bin(char c);
 void bit_sort();
 void load_int(int num);
 void print_buf(char *filename);
-int* read_numbers(FILE *fp);
+void read_numbers(FILE *fp);
+void printb(char c);
 
 int main(int argc, char **argv)
 {
-    int *p;
-    char *filename = "numbers.txt";
-    FILE *fp = fopen(filename, "r");
-    int i;
-    for(i = 0; i < TIMES; i++)
+    if(argc < 2)
     {
-        p = read_numbers(fp);
-        bit_sort(p);
+        printf("Usage: <cmd> <filename>\n");
+        exit(1);
+    }
+    printf("sizeof buf = %d\n", sizeof(buf));
+
+
+    int *p;
+    char *filename = argv[1];
+    FILE *fp = fopen(filename, "r");
+    if(fp == NULL)
+    {
+        printf("open %s failure: ", filename);
+        perror("");
+        exit(1);
+    }
+    int ti;
+    for(ti = 0; ti < TIMES; ti++)
+    {
+        read_numbers(fp);
+        bit_sort();
+        
     }
 
     print_buf("out.txt");
 }
 
-void bit_sort(int* a)
+void bit_sort()
 {
     int i;
     for(i = 0; i < MAX_ONE_TIME; i++)
     {
-        load_int(a[i]);
+        load_int(arr[i]);
     }
 }
 
 void load_int(int num)
 {
+    if(num == 4)
+    {
+        int a = num;
+        char ccc;
+    }
     int m = num / 8;
     int n = num % 8;
     char c = buf[m];
@@ -50,14 +73,14 @@ void print_buf(char *filename)
     int i;
     int j;
     char c;
-    int fd = open(filename, O_WRONLY);
-    if(fd < 0)
+    FILE *fp = fopen(filename, "w");
+    if(fp == NULL)
     {
         perror("open error");
         exit(1);
     }
     int num;
-    char temp[9];
+    char temp[8];
     for (i = 0; i < MAX; i++)
     {
         c = buf[i];
@@ -67,14 +90,14 @@ void print_buf(char *filename)
             if(c & (1 << j))
             {
                 num = i * 8 + j;
-                sprintf(temp, "%d\0", num);
-                write(fd, temp, strlen(temp));
-                write(fd, "\n", 1);
+                sprintf(temp, "%d", num);
+                fputs(temp, fp);
+                fputs("\n", fp);
             }
         }
 
     }
-    close(fd);
+    fclose(fp);
 }
 
 void print_bin(char c)
@@ -94,9 +117,9 @@ void print_bin(char c)
     printf("\n");
 }
 
-int* read_numbers(FILE *fp)
+void read_numbers(FILE *fp)
 {
-    char temp[9];
+    char temp[9];  // 最后一位填\0, 倒数第二位填\n， 剩余7位对应最大7位数，n < 10000000
     int num;
     if(fp == NULL)
     {
@@ -104,13 +127,25 @@ int* read_numbers(FILE *fp)
         exit(1);
     }
 
-    int *pint = (int*)malloc(sizeof(int) * MAX_ONE_TIME);
     int i = 0;
     while(i < MAX_ONE_TIME)
     {
-        fgets(temp, 8, fp);
+        fgets(temp, 9, fp);
         num = atoi(temp);
-        pint[i++] = num;
+        arr[i++] = num;
     }
-    return pint;
+}
+
+void printb(char c)
+{
+    char temp[8];
+    int i;
+    for (i = 0; i < 8; i++)
+    {
+        temp[i] = c % 2;
+        c = c << 1;
+    }
+    for (i = 7; i >= 0; i--)
+        printf("%d", temp[i]);
+    printf("\n");
 }
